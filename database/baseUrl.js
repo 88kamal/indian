ee321dc6a351d4fda18aa0c1ccfd7ecded60fe39
KAMAL NAYAN UPADHYAY <knupadhyay784@gmail.com>
2022-11-05 23:25:57 +0530

    done

--- database/baseUrl.js
+++ database/baseUrl.js
@@ -1,4 +1,4 @@
-const baseUrl = process.env.NODE_ENV === 'production'? "https://indian.vercel.app" : "http://localhost:3000"
+const baseUrl = process.env.NODE_ENV === 'production'? "https://indian-eight.vercel.app" : "http://localhost:3000"
 
 export default  baseUrl
 
